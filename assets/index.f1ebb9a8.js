2a7af641d984a29b41a0d55fa8941986fc9ff4d6
zhanghaibiao4913 <zhanghaibiao4913@users.noreply.github.com>
2022-03-16 03:10:17 +0000

    deploy: a6b4f9d67f1114ec89923fc202c2cfd6a5c72cc6

--- assets/index.f1ebb9a8.js
+++
@@ -1,1 +0,0 @@
-import{d as s,o as e,c as a,j as c,u as l}from"./vendor.ca0f639b.js";const t={class:"full flex justify-center items-center"},r={class:"align-center"},o=c("p",{class:"bold fs30"},"Welcome!",-1);var f=s({setup:s=>(s,f)=>(e(),a("div",t,[c("div",r,[c("img",{src:l("./assets/welcome.b828b5f0.png"),width:"600"},null,8,["src"]),o])]))});export{f as default};
